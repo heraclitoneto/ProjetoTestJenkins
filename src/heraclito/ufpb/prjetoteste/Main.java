55ddf19649e80167343402886cbdb7cd6a526c82
Heráclito Souza <heraclitosouza@MacBook-Pro-de-Heraclito-Souza.local>
2014-03-14 17:45:19 -0300

    add código

---
+++ src/heraclito/ufpb/prjetoteste/Main.java
@@ -0,0 +1,12 @@
+package heraclito.ufpb.prjetoteste;
+
+public class Main {
+	
+	public static void main (String [] args) {
+		
+		Numeros n = new Numeros();
+		
+		n.avaliaNumeros();
+	}
+
+}
---
+++ src/heraclito/ufpb/prjetoteste/Numeros.java
@@ -0,0 +1,19 @@
+package heraclito.ufpb.prjetoteste;
+
+public class Numeros {
+
+	
+	public void avaliaNumeros() {
+	
+	for (int i=1; i<= 50; i++) {
+		if (i % 2 == 0) {
+			System.out.println(i + " - " +"par");
+		}else{
+			System.out.println(i + " - " +"impar");
+		}
+		
+	}
+}
+
+
+}
