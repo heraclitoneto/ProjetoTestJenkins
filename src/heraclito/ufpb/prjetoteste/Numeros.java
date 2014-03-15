687c2c7e709050656471f3386e68618c91aeda30
Heráclito Souza <heraclitosouza@MacBook-Pro-de-Heraclito-Souza.local>
2014-03-14 21:24:40 -0300

    induzindo falha

--- src/heraclito/ufpb/prjetoteste/Numeros.java
+++ src/heraclito/ufpb/prjetoteste/Numeros.java
@@ -6,7 +6,7 @@ public class Numeros {
 	public void avaliaNumeros() {
 	
 	for (int i=1; i<= 50; i++) {
-		if (i % 2 == 0) {
+		if (i  2 == 0) {
 			System.out.println(i + " - " +"par");
 		}else{
 			System.out.println(i + " - " +"impar");
